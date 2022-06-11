b724764b0dae2b266776b3e984e765c7ec3c48be
Md Anas Sabah <mdanassabah@gmail.com>
2022-06-11 22:28:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-       
+      <header>
+        <h1>Profit/Loss</h1>
+      </header>
+      <div className="input_container">
+        <div className="number flex">
+          <h4>Price per stock at the time of purchase: </h4>
+          <input type="number" />
+        </div>
+        <div className="number flex">
+          <h4>Total number of stock purchased: </h4>
+          <input type="number" />
+        </div>
+        <div className="number flex">
+          
+        </div>
+      </div>
     </div>
   );
 }
